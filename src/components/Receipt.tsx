a01d3a40e295d99365dee1ee027378226ea25363
NikolaiMarkalainen <nikolai.markalainen@gmail.com>
2025-01-17 12:11:05 +0200

    Adjusted buttons

--- src/components/Receipt.tsx
+++ src/components/Receipt.tsx
@@ -3,5 +3,29 @@ import { useLocalizedStrings } from "../hooks/hooks";
 
 export const Receipt = () => {
   const strings = useLocalizedStrings();
-  return <div className="receipt-parent">{strings.RECEIPT.TITLE}</div>;
+  return (
+    <div className="receipt-parent">
+      <div className="receipt-header">{strings.RECEIPT.TITLE}</div>
+      <div className="receipt-item">
+        {strings.RECEIPT.CART}
+        <p>10.90$</p>
+      </div>
+      <div className="receipt-item">
+        {strings.RECEIPT.DELIVERY.FEE}
+        <p>5$</p>
+      </div>
+      <div className="receipt-item">
+        {strings.RECEIPT.DELIVERY.DISTANCE}
+        <p>177m</p>
+      </div>
+      <div className="receipt-item">
+        {strings.RECEIPT.SMALL_CHARGE}
+        <p>2.30$</p>
+      </div>
+      <div className="receipt-item">
+        {strings.RECEIPT.TOTAL}
+        <p>100$</p>
+      </div>
+    </div>
+  );
 };
